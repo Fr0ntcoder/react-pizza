76e177ad90829bcbfc57b2d67b88ff2a769c2065
Fr0ntcoder <jambox228@gmail.com>
2022-07-26 12:57:50 +0800

    Updates

---
+++ static/js/NotFound.60232fad.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_pizza=self.webpackChunkreact_pizza||[]).push([[187],{4949:function(e,a,r){r.r(a);r(2791);var t=r(553),u=r(184);a.default=function(){return(0,u.jsx)(t.vr,{})}}}]);
+//# sourceMappingURL=NotFound.60232fad.chunk.js.map
